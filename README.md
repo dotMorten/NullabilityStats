9f025c58a6de4ce5f43aae78f80975c093c91a77
Morten <mnielsen@esri.com>
2020-03-11 14:58:23 -0700

    Cleanup + add csv output

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NullabilityStats
 Commandline tool that gives an estimate per-folder how many lines of code hasn't been nullability annotated
+
+Assumes the entire project has Nullability turned on, and counts files where nullability support has been turned off.
+Does a best guess estimate on lines of code (ignoring usings, namespace, `{}`, empty lines and comments), and gives stats per-folder to be able to measure progress on adding nullability annotations.
--- src/Program.cs
+++ src/Program.cs
@@ -24,28 +24,109 @@ namespace nullstats
     {
         static void Main(string[] args)
         {
-            string[] folders =
+            string[] folders = null;
+            var parameters = ParseArguments(args);
+            if (parameters.ContainsKey("h") || parameters.ContainsKey("help") || parameters.ContainsKey("?"))
             {
-                @"e:\apps\dotnet\dotnet-api\src\Esri.ArcGISRuntime",
-                @"e:\apps\dotnet\dotnet-api\src\Esri.ArcGISRuntime.Hydrography",
-                @"e:\apps\dotnet\dotnet-api\src\LocalServer",
-                @"e:\apps\dotnet\dotnet-api\src\Esri.ArcGISRuntime.Preview",
-            };
+                Console.WriteLine("Parameters:");
+                Console.WriteLine("    -d=[n] : Max Folder depth to report stats for.");
+                Console.WriteLine("    -f=[folder] : Folder to processs (separate by ; or use multiple -f parameters for several folders). Default is current directory.");
+                Console.WriteLine("    -c=[filename] : File name for CSV report");
+                return;
+            }
+            if (parameters.ContainsKey("f"))
+            {
+                folders = parameters["f"];
+            }
+            if(folders == null || folders.Length == 0)
+            {
+                folders = new[] { "." };
+            }
+
+            int depth = -1;
+            if (parameters.ContainsKey("d"))
+            {
+                if (int.TryParse(parameters["d"].FirstOrDefault(), out int d))
+                    depth = d;
+            }
+            StreamWriter csvFile = null;
+            if(parameters.ContainsKey("c"))
+            {
+                csvFile = new StreamWriter(File.OpenWrite(parameters["c"].First()));
+                csvFile.WriteLine("Name,Fraction,LinesOfCode,LinesEnabled,LinesDisabled");
+            }
             List<Stats> stats = new List<Stats>();
             foreach(var folder in folders)
             {
                 var s = new Stats() { Name = folder };
                 GetStats(folder, s);
                 stats.Add(s);
-                PrintResults(s, 0);
+                PrintResults(s, 0, depth);
+                
+                if (csvFile != null)
+                    ExportResults(csvFile, s, new DirectoryInfo(folder).FullName, 0, depth);
             }
+            csvFile?.Dispose();
         }
 
-        private static void PrintResults(Stats s, int indent)
+        private static Dictionary<string,string[]> ParseArguments(string[] args)
         {
-            if (indent > 0)
+            Dictionary<string, string[]> result = new Dictionary<string, string[]>();
+            for (int i = 0; i < args.Length; i++)
             {
-                for (int i = 0; i < indent; i++)
+                var parameter = args[i];
+                string key = null;
+                string value = null;
+                if(parameter.StartsWith("--"))
+                    key = parameter.Substring(2);
+                else if (parameter.StartsWith('-') || parameter.StartsWith('/'))
+                    key = parameter.Substring(1);
+
+                var equals = key.IndexOf('=');
+                if(equals > 0)
+                {
+                    value = key.Substring(equals + 1);
+                    key = key.Substring(0, equals);
+                }
+                else
+                {
+                    if(args.Length > i + 1)
+                    {
+                        i++;
+                        value = args[i];
+                    }
+                }
+
+                string[] values = null;
+                if (value != null)
+                {
+                    if (value.Contains(';'))
+                    {
+                        {
+                            values = value.Split(';');
+                        }
+                    }
+                    else
+                    {
+                        values = new string[] { value };
+                    }
+                }
+                if (values != null && result.ContainsKey(key) && result[key] != null)
+                {
+                    values = result[key].Concat(values).ToArray();
+                }
+                result[key] = values;
+            }
+            return result;
+        }
+
+        private static void PrintResults(Stats s, int currentDepth, int maxDepth)
+        {
+            if (maxDepth > -1 && currentDepth > maxDepth)
+                return;
+            if (currentDepth > 0)
+            {
+                for (int i = 0; i < currentDepth; i++)
                 {
                     Console.Write("  ");
                 }
@@ -61,8 +142,28 @@ namespace nullstats
             Console.ResetColor();
             if (s.Percentage < 100)
             {
-                foreach (var sf in s.Subfolders.OrderBy(s=>s.Disabled))
-                    PrintResults(sf, indent + 1);
+                foreach (var sf in s.Subfolders.OrderBy(s => s.Name))
+                    PrintResults(sf, currentDepth + 1, maxDepth);
+            }
+        }
+        private static void ExportResults(StreamWriter sw, Stats s, string folderName, int currentDepth, int maxDepth)
+        {
+            if (maxDepth == currentDepth)
+            {
+                sw.WriteLine($"\"{folderName}\",{s.Percentage.ToString("0.##")},{s.Count},{s.Enabled},{s.Disabled}");
+            }
+            else
+            {
+                var folderCount = s.Count - s.Subfolders.Sum(s => s.Count);
+                if (folderCount > 0)
+                {
+                    var enabled = s.Enabled - s.Subfolders.Sum(s => s.Enabled);
+                    var disabled = s.Disabled - s.Subfolders.Sum(s => s.Disabled);
+                    var percentage = enabled * 100d / folderCount;
+                    sw.WriteLine($"\"{folderName}\",{percentage.ToString("0.##")},{folderCount},{enabled},{disabled}");
+                }
+                foreach (var sf in s.Subfolders.OrderBy(s => s.Name))
+                    ExportResults(sw, sf, $"{folderName}\\{sf.Name}", currentDepth + 1, maxDepth);
             }
         }
 
